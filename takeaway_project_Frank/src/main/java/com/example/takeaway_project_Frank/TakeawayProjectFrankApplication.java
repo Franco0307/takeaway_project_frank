38c97426363f1712dc7741e2c7ef09e12649c94f
Ella Kitchen <z.yang110817@gmail.com>
2025-01-28 14:49:31 +0000

    takeaway_parent_project create

---
+++ takeaway_project_Frank/src/main/java/com/example/takeaway_project_Frank/TakeawayProjectFrankApplication.java
@@ -0,0 +1,13 @@
+package com.example.takeaway_project_Frank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeawayProjectFrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeawayProjectFrankApplication.class, args);
+	}
+
+}
