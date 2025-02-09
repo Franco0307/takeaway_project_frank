251fb0985789b3cd0bed27a8c7bd6c8c511ea73a
Ella Kitchen <z.yang110817@gmail.com>
2025-02-09 20:48:50 +0000

    RedisConfiguration+ controller for SetMeal and Dish

---
+++ springboot_multiple_module_demo/sky-server/src/main/java/com/ellaskitchen/controller/admin/SetMealController.java
@@ -0,0 +1,100 @@
+package com.ellaskitchen.controller.admin;
+
+import com.ellaskitchen.dto.SetMealDTO;
+import com.ellaskitchen.dto.SetMealPageQueryDTO;
+import com.ellaskitchen.result.PageResult;
+import com.ellaskitchen.result.Result;
+import com.ellaskitchen.service.SetMealService;
+import com.ellaskitchen.vo.SetMealVO;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping
+@Slf4j
+public class SetMealController {
+    @Autowired
+    private SetMealService setMealService;
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealDTO.categoryId")
+    public Result save(@RequestBody SetMealDTO setmealDTO) {
+        setMealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(SetMealPageQueryDTO setmealPageQueryDTO) {
+        PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result delete(@RequestParam List<Long> ids) {
+        setMealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<SetMealVO> getById(@PathVariable Long id) {
+        SetMealVO setmealVO = setMealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result update(@RequestBody SetMealDTO setmealDTO) {
+        setMealService.update(setmealDTO);
+        return Result.success();
+    }
+
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售停售")
+    @CacheEvict(cacheNames = "setmealCache",allEntries = true)
+    public Result startOrStop(@PathVariable Integer status, Long id) {
+        setMealService.startOrStop(status, id);
+        return Result.success();
+    }
+}
